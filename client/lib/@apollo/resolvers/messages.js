61b26c26b37466f91381f709a040b464790abc16
DeCryptinWeb <info@ivanvinas.ch>
2018-05-15 17:08:49 +0200

    [Random] - increased code-beauty

--- client/lib/@apollo/resolvers/messages.js
+++ client/lib/@apollo/resolvers/messages.js
@@ -1,10 +1,10 @@
 import gql from 'graphql-tag'
 
 export default {
-    Mutation: {
-        addNewMessageToState: (_, {type, text, topic}, {cache}) => {
-            // 
-            const query = gql`
+  Mutation: {
+    addNewMessageToState: (_, {type, text, topic}, {cache}) => {
+      //
+      const query = gql`
                 query getMessages {
                     messagesState @client {
                         messages {
@@ -17,26 +17,26 @@ export default {
                 }
             `
 
-            const prevState = cache.readQuery({query})
+      const prevState = cache.readQuery({query})
 
-            const newState = {
-                ...prevState,
-                messagesState: {
-                    ...prevState.messagesState,
-                    messages: [
-                        ...prevState.messagesState.messages,
-                        {
-                            id: prevState.messagesState.messages.length + 1,
-                            text,
-                            type,
-                            topic,
-                            __typename: 'message'
-                        } 
-                    ]
-                }
+      const newState = {
+        ...prevState,
+        messagesState: {
+          ...prevState.messagesState,
+          messages: [
+            ...prevState.messagesState.messages,
+            {
+              id: prevState.messagesState.messages.length + 1,
+              text,
+              type,
+              topic,
+              __typename: 'message'
             }
-
-            cache.writeData({query, data: newState})
+          ]
         }
+      }
+
+      cache.writeData({query, data: newState})
     }
+  }
 }
